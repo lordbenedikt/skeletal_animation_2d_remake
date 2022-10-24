3d07bf535b4237cc28efa0039216c1b6df23c5a8
Benedikt Jensen <benedikt.jensen@mni.thm.de>
2022-10-24 20:19:22 +0200

    testing

---
+++ load-animations.js
@@ -0,0 +1,34 @@
+  var loaded_anim = "nothing";
+
+  function dateiauswahl(evt) {
+console.log("auswahl");
+    var dateien = evt.target.files; // FileList object
+    // Auslesen der gespeicherten Dateien durch Schleife
+    for (var i = 0, f; f = dateien[i]; i++) {
+      // // nur .Text-Dateien
+      // if (!f.na.match('txt.*')) {
+      // 	continue;
+      // }
+      var reader = new FileReader();
+      reader.onload = (function (theFile) {
+        return function (e) {
+          fetch(e.target.result)
+            .then(function (response) {
+              response.text().then(function (text) {
+                loaded_anim = text;
+                done();
+              });
+            });
+        };
+      })(f);
+      // Als Data URL auslesen.
+      reader.readAsDataURL(f);
+    }
+  }
+  function done() {
+    console.log(loaded_anim);
+  }
+  // Auf neue Auswahl reagieren und gegebenenfalls Funktion dateiauswahl neu ausführen.
+  let element = document.getElementById('files');
+
+  element.addEventListener('change', dateiauswahl, false);
