323f6a2fcdfdcbe78df45f082c437757910077c2
Benedikt Jensen <benedikt.jensen@mni.thm.de>
2022-10-25 00:43:58 +0200

    bugfix

--- load-animations.js
+++ load-animations.js
@@ -1,34 +1,36 @@
-  export function uploadFileToLocalStorage() {
-    //new
-    document.getElementById('files').click();
-  }
-  function dateiauswahl(evt) {
-console.log("auswahl");
-    var dateien = evt.target.files; // FileList object
-    // Auslesen der gespeicherten Dateien durch Schleife
-    for (var i = 0, f; f = dateien[i]; i++) {
-      // // nur .Text-Dateien
-      // if (!f.na.match('txt.*')) {
-      // 	continue;
-      // }
-      var reader = new FileReader();
-      reader.onload = (function (theFile) {
-        return function (e) {
-          fetch(e.target.result)
-            .then(function (response) {
-              response.text().then(function (text) {
-                window.localStorage.setItem("loaded_anim", text);
-                window.localStorage.setItem("loaded_anim", parseInt(window.localStorage.getItem("loaded_anim")) + 1);
-              });
-            });
-        };
-      })(f);
-      // Als Data URL auslesen.
-      reader.readAsDataURL(f);
-    }
-  }
-  
-  // Auf neue Auswahl reagieren und gegebenenfalls Funktion dateiauswahl neu ausführen.
-  let element = document.getElementById('files');
+export function uploadFileToLocalStorage() {
+  //new
+  document.getElementById('files').click();
+}
 
-  element.addEventListener('change', dateiauswahl, false);
+function dateiauswahl(evt) {
+  console.log("auswahl");
+  var dateien = evt.target.files; // FileList object
+  // Auslesen der gespeicherten Dateien durch Schleife
+  for (var i = 0, f; f = dateien[i]; i++) {
+    // // nur .Text-Dateien
+    // if (!f.na.match('txt.*')) {
+    // 	continue;
+    // }
+    var reader = new FileReader();
+    reader.onload = (function (theFile) {
+      return function (e) {
+        fetch(e.target.result)
+          .then(function (response) {
+            response.text().then(function (text) {
+              window.localStorage.setItem("loaded_anim", text);
+              window.localStorage.setItem("loaded_count", parseInt(window.localStorage.getItem("loaded_count")) + 1);
+            });
+          });
+      };
+    })(f);
+    // Als Data URL auslesen.
+    reader.readAsDataURL(f);
+  }
+}
+
+window.localStorage.setItem("loaded_count", 0);
+
+// Auf neue Auswahl reagieren und gegebenenfalls Funktion dateiauswahl neu ausführen.
+let element = document.getElementById('files');
+element.addEventListener('change', dateiauswahl, false);
