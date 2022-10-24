dd55f277a57dff4b35bc33dfdc140c947f426f76
Benedikt Jensen <benedikt.jensen@mni.thm.de>
2022-10-25 00:31:48 +0200

    layout improvement

--- src/egui.rs
+++ src/egui.rs
@@ -47,7 +47,6 @@ pub struct State {
     pub delaunay_borderline_width: f32,
     pub adjust_vertex_weights_mode: bool,
     pub brush_size: f32,
-    pub debug_message: String,
     pub save_filename: String,
 }
 impl Default for State {
@@ -71,7 +70,6 @@ impl Default for State {
             delaunay_borderline_width: 3.,
             adjust_vertex_weights_mode: false,
             brush_size: 0.5,
-            debug_message: String::new(),
             save_filename: String::new(),
         }
     }
@@ -702,24 +700,27 @@ pub fn animation_menu(
     let response = egui::Window::new("Animations")
         .resizable(false)
         .show(egui_context.ctx_mut(), |ui| {
-            // #[cfg(target_arch = "wasm32")]
-            ui.horizontal(|ui| {
+            #[cfg(target_arch = "wasm32")]
+            {
                 ui.text_edit_singleline(&mut state.save_filename);
-                if ui.button("Save to local disc").clicked() {
-                    save_evw.send(SaveEvent(state.save_filename.clone()));
-                }
-                if ui.button("Load locally saved file").clicked() {
-                    #[link(wasm_import_module = "./load-animations.js")]
-                    extern "C" {
-                        fn uploadFileToLocalStorage();
+                ui.horizontal(|ui| {
+                    if ui.button("Save to local disc").clicked() {
+                        save_evw.send(SaveEvent(state.save_filename.clone()));
                     }
-                    unsafe {
-                        uploadFileToLocalStorage();
+                    if ui.button("Load locally saved file").clicked() {
+                        #[link(wasm_import_module = "./load-animations.js")]
+                        extern "C" {
+                            fn uploadFileToLocalStorage();
+                        }
+                        unsafe {
+                            uploadFileToLocalStorage();
+                        }
                     }
-                }
-            });
+                });
 
-            ui.label(state.debug_message.clone());
+                ui.seperator();
+            }
+
             animations_all(
                 ui,
                 &mut state,
